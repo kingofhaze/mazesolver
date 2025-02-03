760d73363afc452a16bca058417ea247821ba645
kingofhaze <194982118+kingofhaze@users.noreply.github.com>
2025-02-02 21:00:17 -0500

    make a square

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from window import Window
+from line import Line
+from point import Point
 
 def main():
     win = Window(800, 600)
+
+    win.draw_line(Line(Point(200,200), Point(400,200)), "black")
+    win.draw_line(Line(Point(400,200), Point(400,400)), "black")
+    win.draw_line(Line(Point(400,400), Point(200,400)), "black")
+    win.draw_line(Line(Point(200,400), Point(200,200)), "black")
+    
     win.wait_for_close()
 
 if __name__ == "__main__":
